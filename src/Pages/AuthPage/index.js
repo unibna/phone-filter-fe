4b742af33b2b1a1e2e15e57ef96bf26e4d8a5a58
lan nguyen <lan.nguyen@seedcom.vn>
2024-04-15 05:27:08 +0700

    deployment: add nginx conf

--- src/Pages/AuthPage/index.js
+++ src/Pages/AuthPage/index.js
@@ -1,17 +1,9 @@
-import { useState } from 'react';
-
 import LoginPage from './LoginPage';
-import RegisterPage from './RegisterPage';
-
-import AuthService from '../../Services/AuthService';
-
 
 const AuthPage = () => {
-    const currentUser = AuthService.getCurrentUser();
-
     return (
         <div>
-            {currentUser ? <LoginPage /> : <RegisterPage />}
+            <LoginPage />
         </div>
     );
 };
